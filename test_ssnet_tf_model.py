0e2b56157b7a6016e374f570b76554d7df41938e
Taritree <taritree.wongjirad@gmail.com>
2018-06-16 14:43:44 -0400

    add savemodel commands

--- test_ssnet_tf_model.py
+++ test_ssnet_tf_model.py
@@ -3,6 +3,8 @@ import os,sys
 import numpy as np
 import tensorflow as tf
 
+
+
 from ssnet_tf_code_plane0 import UResNet
 
 data_p0 = np.load("ssnet_tf_data_plane0.npy")
@@ -14,14 +16,16 @@ print "Net defined"
 
 blank = np.zeros( (1,512,512,1), dtype=np.float32 )
 
-with tf.Session() as sess:
+sess = tf.Session()
 
-    # load the model
-    unet.load( "ssnet_tf_data_plane0.npy", sess )
+# load the model
+unet.load( "ssnet_tf_data_plane0.npy", sess )
 
-    # define the folder to export the SavedModel
-    export_dir = "export"
+# define the folder to export the SavedModel
+export_dir = "export"
 
-    pred = sess.run(unet.get_output(), feed_dict={data_node:blank})
+# run it
+pred = sess.run(unet.get_output(), feed_dict={data_node:blank})
 
-
+# save the model (for serving?)
+tf.saved_model.simple_save( sess, "SaveModelSSNet", inputs={"uplane":data_node}, outputs={"ussnet":pred} )
